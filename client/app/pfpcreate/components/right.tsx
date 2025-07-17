01aca8da4e519340937196677f2bad953382f1d1
diyajojo <diyaj2122@gmail.com>
2025-07-18 02:59:32 +0530

    notifs,likes and centered upload pic section

--- client/app/pfpcreate/components/right.tsx
+++ client/app/pfpcreate/components/right.tsx
@@ -9,7 +9,7 @@ export default function PfpRightSide() {
     <div className="relative flex flex-col items-start justify-start pt-10 pl-10 bg-black h-full w-full">
       {/* Upload Photo container */}
       <div
-        className="relative rounded-[30px] overflow-hidden"
+        className="relative mx-60 rounded-[30px] overflow-hidden"
         style={{
           width: "581px",
           height: "450px",
@@ -42,20 +42,53 @@ export default function PfpRightSide() {
         </div>
       </div>
 
-      {/* Upgrade Account Button */}
-      <button
-        type="button"
-        className="absolute top-[73px] right-10 text-white font-fjalla-one text-xl border border-white"
-        style={{
-          width: "300px",
-          height: "55px",
-          borderRadius: "20px",
-          background: "#CC90DA",
-          boxShadow: "12px 12px 20px 0px #00000040",
-        }}
-      >
-        Upgrade Account
-      </button>
+      {/* Top Action Bar */}
+      <div className="absolute top-[73px] right-10 flex items-center gap-5">
+        {/* Upgrade */}
+        <button
+          type="button"
+          className="text-white font-fjalla-one text-xl border border-white"
+          style={{
+            width: "300px",
+            height: "55px",
+            borderRadius: "20px",
+            background: "#CC90DA",
+            boxShadow: "12px 12px 20px 0px #00000040",
+          }}
+        >
+          Upgrade Account
+        </button>
+
+        {/* Bell Icon */}
+        <button type="button" className="relative">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="#FFFFFF"
+            className="w-8 h-8"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V4a2 2 0 10-4 0v1.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+          </svg>
+          {/* green dot */}
+          <span className="absolute top-0 right-0 block w-2.5 h-2.5 bg-green-400 rounded-full"></span>
+        </button>
+
+        {/* Heart Icon */}
+        <button type="button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#FFFFFF"
+            strokeWidth={2}
+            className="w-8 h-8"
+          >
+            <path d="M12 21s-4.35-2.54-7.2-5.3C2 13 1 10.54 1 8.5 1 5.42 3.42 3 6.5 3c1.74 0 3.41.81 4.5 2.09C12.09 3.81 13.76 3 15.5 3 18.58 3 21 5.42 21 8.5c0 2.04-1 4.5-3.8 7.2C16.35 18.46 12 21 12 21z" />
+          </svg>
+        </button>
+      </div>
 
       {/* Profile Info Form */}
       <form className="mt-16 w-full max-w-5xl text-white font-fjalla-one">
